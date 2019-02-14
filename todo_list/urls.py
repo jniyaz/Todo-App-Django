e06e6a55f7335030aa0a1ae301f18b6cc73f90cb
Niyaz <niyaz@jocom.my>
2019-02-14 14:33:23 +0800

    task completed/not completed implementation done

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('delete/<list_id>', views.delete, name="delete"),
-    path('status_completed/<list_id>', views.delete, name="status_completed"),
-    path('status_not_completed/<list_id>', views.delete, name="status_not_completed"),
+    path('status_completed/<list_id>', views.status_completed, name="status_completed"),
+    path('status_not_completed/<list_id>', views.status_not_completed, name="status_not_completed"),
 ]
