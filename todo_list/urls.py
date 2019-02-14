4937c312063922f35c583be396f717e101bdd2ab
Niyaz <niyaz@jocom.my>
2019-02-14 12:38:54 +0800

    status updates added

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('delete/<list_id>', views.delete, name="delete"),
+    path('status_completed/<list_id>', views.delete, name="status_completed"),
+    path('status_not_completed/<list_id>', views.delete, name="status_not_completed"),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -6,13 +6,11 @@ from django.contrib import messages
 def home(request):
     if request.method == 'POST':
         form = ListForm(request.POST or None)
-
         if form.is_valid():
             form.save()
             all_items = List.objects.all
             messages.success(request, ('Item has been added to list.'))
             return render(request, 'home.html', {'all_items' : all_items})
-
     else:
         all_items = List.objects.all
         return render(request, 'home.html', {'all_items' : all_items})
