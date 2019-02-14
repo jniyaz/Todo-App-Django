03d990cd62e3298b0ccc34fb0051a5de2722ea59
Niyaz <niyaz@jocom.my>
2019-02-14 12:23:40 +0800

    list model, form creation implementations done

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item has been added to list.'))
+            return render(request, 'home.html', {'all_items' : all_items})
+
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items' : all_items})
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Task has been deleted'))
+    return redirect('home')
+
+def status_completed(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    messages.success(request, ('Task has been marked as completed'))
+    return redirect('home')
+
+def status_not_completed(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    messages.success(request, ('Task has been marked as not completed'))
+    return redirect('home')
